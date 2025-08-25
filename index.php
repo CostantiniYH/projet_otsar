65511102f1afd194a93155b6bd5b15751f02a55a
CostantiniYH <costantiniyaacov@gamil.com>
2025-08-26 00:39:37 +0200

    Voir les erreurs

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
 require_once __DIR__ . '/backend/db_connect.php';
 require_once __DIR__ . '/controllers/session.php';
 require_once __DIR__ . '/components/header.php';

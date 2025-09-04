86c208b8427505cd4a94d586ba84a36684c76fc9
CostantiniYH <costantiniyaacov@gamil.com>
2025-09-04 21:36:34 +0200

    Form/Compte

--- components/card_livre.php
+++ components/card_livre.php
@@ -1,9 +1,6 @@
 <div class="card border-0 shadow hvr-shadow-radial"  style="width: 18rem;">
-  <?php
-
-  $test = $value['nom_path'] ?>
-  <img src="<?= BASE_URL . $value['image']; ?>" class="card-img-top card-img" 
-  alt="<?= $value['titre']; ?>" style="height: 200px; object-fit: cover;" usemap="#map<?= $value['id']; ?>">
+  <img src="<?= BASE_URL . 'uploads/' . $value['nom_path'] . '/' . $value['image']; ?>" class="card-img-top 
+  card-img" alt="<?= $value['titre'];?>" style="height: 200px; object-fit: cover;" usemap="#map<?= $value['id'];?>">
   <map name="map<?= $value['id']; ?>">
     <area shape="rect" coords="0,0,300,200" href="<?= BASE_URL ?>livre_one.php?id=<?= $value['id']; ?>">
   </map>

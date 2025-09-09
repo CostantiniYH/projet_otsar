01bafeae81a4e9703f3b2ddb9e06013dbf23d493
CostantiniYH <costantiniyaacov@gamil.com>
2025-09-09 12:17:15 +0200

    auto deploy

--- components/image.php
+++ components/image.php
@@ -1,4 +1,4 @@
 <div class="card shadow">
-    <img src="<?= $value['chemin']; ?>" class="card-img" />
+    <img src="<?= BASE_URL . $value['chemin']; ?>" class="card-img" />
     <p class="carousel-caption"><?= $value['nom']; ?></p>
 </div>
